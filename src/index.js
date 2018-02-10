04a6b56fcdf775e44460d2f04282e80d9c61079a
edgarsrn <edgarsrn@users.noreply.github.com>
2018-02-10 16:11:54 +0200

    add function isShmita()

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ module.exports = {
     isLeapYear: function (year) {
         var GUX_ADZAT = [3, 6, 8, 11, 14, 17, 0];
         return GUX_ADZAT.includes(year % 19);
+    },
+
+    isShmita: function (year) {
+        return year % 7;
     }
 };
